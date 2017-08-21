190ff2064734d7fdef168efc01fd2aa60197aaa7
K.Bailey <katebailey1230@gmail.com>
2017-08-21 14:17:21 -0400

    22 examples, 9 failures

--- binary_tree/binary_search_tree.rb
+++ binary_tree/binary_search_tree.rb
@@ -8,25 +8,59 @@ class BinarySearchTree
   end
 
   def insert(root, node)
-   if root = nil
-    root = Node.new(data)
-    
-    
+   temp = root
+   
+   while temp
+    if node.rating < temp.rating
+     if temp.left
+      temp = temp.left
+     else
+      temp.left = node
+      break
+     end
+    end
+   
+    if node.rating > temp.rating
+     if temp.right
+      temp = temp.right
+     else
+      temp.right = node
+      break
+     end
+    end
    end
+  end
    
   # Recursive Depth First Search
   def find(root, data)
-   node = root
-   title = data
-   
-   
+   if root && root.title == data
+    return root
+   end
+    
+   if root.left
+    if root.left.title != data
+     find(root.left, data)
+    else
+     return root.left
+    end
+   elsif root.right
+    if root.right.title != data
+     find(root.right, data)
+    else
+     return root.right
+    end
+   else
+    return nil
+   end
   end
+end
 
   def delete(root, data)
+  
+   #must use parent and child because if you delete parent, child must become parent, etc.
+   
   end
 
   # Recursive Breadth First Search
   def printf(children=nil)
   end
- 
-
