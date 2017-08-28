604c16cccb1409348953df04e0bf7847a3aaf64a
K.Bailey <katebailey1230@gmail.com>
2017-08-28 11:15:33 -0400

    first tree complete

--- binary_tree/binary_search_tree.rb
+++ binary_tree/binary_search_tree.rb
@@ -1,7 +1,7 @@
 require_relative 'node'
 
 class BinarySearchTree
- 
+	
 
   def initialize(root)
    @root = root
@@ -33,49 +33,57 @@ class BinarySearchTree
    
   # Recursive Depth First Search
   def find(root, data)
-   if root && root.title == data
-    return root
-   end
-    
-   if root.left
-    if root.left.title != data
-     find(root.left, data)
-    else
-     return root.left
-    end
-   elsif root.right
-    if root.right.title != data
-     find(root.right, data)
-    else
-     return root.right
-    end
-   else
-    return nil
+   if data === nil
+	   return nil
+   elsif root.title === data
+	   return root
+   elsif root.title !=data && root.left === nil
+	   if root.right === data
+		   return root.right
+	   elsif root.right === nil
+		   return nil
+	   else
+		   find(root.right, data)
+	   end
+   elsif root.title != data && root.left != nil
+	   if root.left === data
+		   return root.left
+	   else
+		   find(root.left, data)
+	   end
    end
   end
-end
+
 
   def delete(root, data)
-	 if root.nil? || data.nil?
-		 return nil
-	 else
-		 target_node = find(root, data)
-		 target_node.nil? ? nil : target_node.title = nil
-	 end
+	  if data.nil?
+		return nil
+	  else
+		 node = self.find(root, data)
+		  if node.nil?
+			  return nil
+		  else
+			  node.title = nil
+			  node.rating = nil
+		  end
+	  end
   end
-   
 
   # Recursive Breadth First Search
   def printf(children=nil)
-	  tree = [root]
-	  tree.each do |node|
-		  tree << node.left if node.left
-		  tree << node.right if node.right
-		  node.left << node.children.left if node.children.left
-		  node.left << node.children.right if node.children.right
-		  node.right << node.children.left if node.children.left
-		  node.right << node.children.right if node.children.right
+	array = [@root]
+	output = []
+	  while array.length > 0
+		  node = array.shift
+		  if node.left
+			  array.push(node.left)
+		  end
+		  if node.right
+			  array.push(node.right)
+		  end
+		  output.push("#{node.title}: #{node.rating}").join "/n"
 	  end
-	  puts tree.inspect 
+	  output.each {|movie| puts movie}
   end
-  
+  end
+
