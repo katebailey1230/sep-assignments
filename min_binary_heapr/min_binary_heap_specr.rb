f1e94a35151fd9c9accd308d4e99da1349a69f4e
K.Bailey <katebailey1230@gmail.com>
2017-09-25 11:41:56 -0400

    1 failure?

--- min_binary_heapr/min_binary_heap_specr.rb
+++ min_binary_heapr/min_binary_heap_specr.rb
@@ -2,6 +2,7 @@ include RSpec
 
 require_relative 'min_binary_heapr'
 
+
 RSpec.describe MinBinaryHeapr, type: Class do
   let (:root) { Node.new("The Matrix", 87) }
 
@@ -19,15 +20,9 @@ RSpec.describe MinBinaryHeapr, type: Class do
   let (:mad_max_2) { Node.new("Mad Max 2: The Road Warrior", 98) }
 
 
-  describe "#initialize" do
-    it "sets @ root to passed node" do
-      tree = MinBinaryHeapr.new(root)
-      expect(tree.elements[0]).to eq root
-    end
-  end
-	
+
   describe "#insert" do
-    it "inserts all nodes into elements array and then sorts by ratings" do
+    it "inserts everything properly" do
       tree.insert(root, pacific_rim)
       tree.insert(root, braveheart)
       tree.insert(root, mad_max_2)
@@ -40,13 +35,12 @@ RSpec.describe MinBinaryHeapr, type: Class do
       tree.insert(root, shawshank)
       tree.insert(root, martian)
 
-      expect(tree.elements[0]).to eq "Pacific Rim"
-      expect(tree.elements[1]).to eq "Braveheart"
+      expect(tree.elements[1].title).to eq "Pacific Rim"
 end
   end
 
   describe "#find(data)" do
-    it "returns nil if data is nil" do
+    it "handles nil gracefully" do
       tree.insert(root, empire)
       tree.insert(root, mad_max_2)
       expect(tree.find(root, nil)).to eq nil
@@ -60,7 +54,6 @@ end
 
 
   describe "#delete(data)" do
-    
     it "properly deletes a node" do
       tree.insert(root, hope)
       tree.delete(root, hope.title)
@@ -72,17 +65,17 @@ end
   describe "#print" do
      specify {
        expected_output = "Pacific Rim: 72\nBraveheart: 78\nStar Wars: Return of the Jedi: 80\nThe Matrix: 87\nDistrict 9: 90\nStar Wars: The Empire Strikes Back: 94\nInception: 86\nStar Wars: A New Hope: 93\nThe Shawshank Redemption: 91\nThe Martian: 92\nMad Max 2: The Road Warrior: 98\n"
-       tree.insert(hope)
-       tree.insert(empire)
-       tree.insert(jedi)
-       tree.insert(martian)
-       tree.insert(pacific_rim)
-       tree.insert(inception)
-       tree.insert(braveheart)
-       tree.insert(shawshank)
-       tree.insert(district)
-       tree.insert(mad_max_2)
-       expect { tree.print }.to output(expected_output).to_stdout
+       tree.insert(root, hope)
+       tree.insert(root, empire)
+       tree.insert(root, jedi)
+       tree.insert(root, martian)
+       tree.insert(root, pacific_rim)
+       tree.insert(root, inception)
+       tree.insert(root, braveheart)
+       tree.insert(root, shawshank)
+       tree.insert(root, district)
+       tree.insert(root, mad_max_2)
+       expect { require_relative 'min_binary_heapr' }.to output(expected_output).to_stdout
        }
   end
 end
